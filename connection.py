995a78d880baa62cde65231b30532a1937190265
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-10 02:51:12 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -5,7 +5,7 @@ def connect_to_db():
             return  pymysql.connect(
                 host = "127.0.0.1",
                 user = "root",
-                password = "Aa36997362",
+                password = "",
                 port = 3306,
                 db = "inventory_db",    # Name of your databse
                 cursorclass=pymysql.cursors.DictCursor  # To get results as dictionaries
